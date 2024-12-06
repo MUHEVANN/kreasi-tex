0db8ecc768a4e1ea18aead3a6029483f03ca8077
evan <evan.kusyanto@students.amikom.ac.id>
2024-12-06 22:31:31 +0700

    add dashboard layout

--- resources/js/Layouts/AdminLayout.tsx
+++ resources/js/Layouts/AdminLayout.tsx
@@ -1,17 +1,51 @@
 import { AppSidebar } from "@/Components/AppSidebar";
-import { SidebarProvider, SidebarTrigger } from "@/Components/ui/sidebar";
-import React from "react";
+import {
+    Breadcrumb,
+    BreadcrumbItem,
+    BreadcrumbLink,
+    BreadcrumbList,
+    BreadcrumbPage,
+    BreadcrumbSeparator,
+} from "@/Components/ui/breadcrumb";
+import { Separator } from "@/Components/ui/separator";
+import {
+    SidebarInset,
+    SidebarProvider,
+    SidebarTrigger,
+} from "@/Components/ui/sidebar";
 
 function AdminLayout({ children }: { children: React.ReactNode }) {
     return (
         <SidebarProvider>
             <AppSidebar />
-            <main>
-                <SidebarTrigger />
-                <div className="lg:py-10 lg:px-12">{children}</div>
-            </main>
+            <SidebarInset>
+                <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12">
+                    <div className="flex items-center gap-2 px-4">
+                        <SidebarTrigger className="-ml-1" />
+                        <Separator
+                            orientation="vertical"
+                            className="mr-2 h-4"
+                        />
+                        <Breadcrumb>
+                            <BreadcrumbList>
+                                <BreadcrumbItem className="hidden md:block">
+                                    <BreadcrumbLink href="#">
+                                        Building Your Application
+                                    </BreadcrumbLink>
+                                </BreadcrumbItem>
+                                <BreadcrumbSeparator className="hidden md:block" />
+                                <BreadcrumbItem>
+                                    <BreadcrumbPage>
+                                        Data Fetching
+                                    </BreadcrumbPage>
+                                </BreadcrumbItem>
+                            </BreadcrumbList>
+                        </Breadcrumb>
+                    </div>
+                </header>
+                {children}
+            </SidebarInset>
         </SidebarProvider>
     );
 }
-
 export default AdminLayout;
