9e810f45b25435a7a3f6594382259151b503d8a7
evan <evan.kusyanto@students.amikom.ac.id>
2024-12-07 22:53:31 +0700

    faq get data

--- resources/js/Components/Faq.tsx
+++ resources/js/Components/Faq.tsx
@@ -1,3 +1,4 @@
+import { get } from "@/lib/api";
 import TitleSection from "./TitleSection";
 import {
     Accordion,
@@ -5,51 +6,54 @@ import {
     AccordionItem,
     AccordionTrigger,
 } from "./ui/accordion";
+import { useEffect, useState } from "react";
+
+const getFaq = async () => {
+    const res = await get("/faq/data");
+    return res.data.data;
+};
+
+type FaqProps = {
+    id: number;
+    question: string;
+    answer: string;
+    created_at: string;
+};
 
 function Faq() {
+    const [data, setData] = useState<FaqProps[]>([]);
+    useEffect(() => {
+        async function fetchData() {
+            const res = await getFaq();
+            setData(res);
+        }
+        fetchData();
+    }, []);
+
     return (
         <div>
             <TitleSection className="text-center ">FAQ</TitleSection>
             <div className="main__container">
-                <Accordion type="single" collapsible className="2xl:px-[20rem]">
-                    <AccordionItem
-                        value="item-1"
-                        className="bg-gray-100 px-[1rem] mb-3"
+                {data.map((item, i) => (
+                    <Accordion
+                        key={i}
+                        type="single"
+                        collapsible
+                        className="2xl:px-[20rem]"
                     >
-                        <AccordionTrigger className="text-lg">
-                            Is it accessible?
-                        </AccordionTrigger>
-                        <AccordionContent className="text-md opacity-75">
-                            Yes. It adheres to the WAI-ARIA design pattern.
-                        </AccordionContent>
-                    </AccordionItem>
-                </Accordion>
-                <Accordion type="single" collapsible className="2xl:px-[20rem]">
-                    <AccordionItem
-                        value="item-1"
-                        className="bg-gray-100 px-[1rem] mb-3"
-                    >
-                        <AccordionTrigger className="text-lg">
-                            Is it accessible?
-                        </AccordionTrigger>
-                        <AccordionContent className="text-md opacity-75">
-                            Yes. It adheres to the WAI-ARIA design pattern.
-                        </AccordionContent>
-                    </AccordionItem>
-                </Accordion>
-                <Accordion type="single" collapsible className="2xl:px-[20rem]">
-                    <AccordionItem
-                        value="item-1"
-                        className="bg-gray-100 px-[1rem] mb-3"
-                    >
-                        <AccordionTrigger className="text-lg">
-                            Is it accessible?
-                        </AccordionTrigger>
-                        <AccordionContent className="text-md opacity-75">
-                            Yes. It adheres to the WAI-ARIA design pattern.
-                        </AccordionContent>
-                    </AccordionItem>
-                </Accordion>
+                        <AccordionItem
+                            value="item-1"
+                            className="bg-gray-100 px-[1rem] mb-3"
+                        >
+                            <AccordionTrigger className="text-lg">
+                                {item.question}
+                            </AccordionTrigger>
+                            <AccordionContent className="text-md opacity-75">
+                                {item.answer}
+                            </AccordionContent>
+                        </AccordionItem>
+                    </Accordion>
+                ))}
             </div>
         </div>
     );
