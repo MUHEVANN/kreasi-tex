d0dd7e259cce44b24a9675572a3183d8b1860410
evan <evan.kusyanto@students.amikom.ac.id>
2024-12-13 22:48:32 +0700

    add navbar

--- resources/js/Components/Navbar.tsx
+++ resources/js/Components/Navbar.tsx
@@ -1,4 +1,5 @@
 import { Link } from "@inertiajs/react";
+import { AlignJustify, X } from "lucide-react";
 import React from "react";
 
 const navLinks = [
@@ -21,15 +22,42 @@ const navLinks = [
 ];
 
 function Navbar() {
+    const [isOpen, setIsOpen] = React.useState(false);
     return (
         <div className="w-full fixed top-0 z-[999]">
-            <div className="md:px-[5rem] lg:px-[15rem] 2xl:px-[30rem] hidden md:block">
+            <div className="md:px-[5rem] lg:px-[15rem] 2xl:px-[30rem] hidden">
                 <div className=" flex justify-between py-8 border-b border-black backdrop-blur-sm">
                     {navLinks.map((link, index) => (
                         <Link
                             key={index}
                             href={link.href}
                             className=" text-[18px] noto-sans"
+                        >
+                            {link.name}
+                        </Link>
+                    ))}
+                </div>
+            </div>
+            <div className="px-4 py-5" onClick={() => setIsOpen(true)}>
+                <AlignJustify className="hover:cursor-pointer" />
+            </div>
+            <div
+                className={`fixed top-0 w-[70%] h-full bg-white z-[999] transition-all duration-300 ease-in-out ${
+                    isOpen ? "left-0" : "-left-full"
+                }`}
+            >
+                <div className="w-full py-4 px-5 flex justify-end">
+                    <X
+                        onClick={() => setIsOpen(false)}
+                        className="hover:cursor-pointer"
+                    />
+                </div>
+                <div>
+                    {navLinks.map((link, index) => (
+                        <Link
+                            key={index}
+                            href={link.href}
+                            className="block text-[18px] noto-sans py-4 px-5  border-gray-200"
                         >
                             {link.name}
                         </Link>
--- resources/js/Layouts/Layout.tsx
+++ resources/js/Layouts/Layout.tsx
@@ -1,12 +1,14 @@
 import Footer from "@/Components/Footer";
 import HeroLogo from "@/Components/HeroLogo";
 import Navbar from "@/Components/Navbar";
+import { AlignJustify } from "lucide-react";
 import React from "react";
 
 function Layout({ children }: { children: React.ReactNode }) {
     return (
         <div>
             <Navbar />
+         
             <div
                 className="min-h-screen  bg-no-repeat bg-cover relative bg-fixed"
                 style={{ backgroundImage: "url('hero.webp')" }}
