55773fa569609b5f016c7d5957c54328c0c33540
irfiyanda_abidin <irfiyanda.abidin@varx.id>
2025-01-07 10:54:40 +0700

    fix card

--- resources/js/Components/ValueCard.tsx
+++ resources/js/Components/ValueCard.tsx
@@ -10,11 +10,11 @@ type ValueCardProps = {
 function ValueCard({ icon, title, desc }: ValueCardProps) {
     return (
         <div className="max-w-[180px] lg:max-w-[25%] xl:max-w-[20%] flex justify-center flex-col items-center gap-4">
-            <div className="p-4 bg-gray-400 rounded-full text-black ">
+            <div className="p-4 bg-yellow-400 rounded-full text-black ">
                 <DynamicIcon iconId={icon} />
             </div>
             <div>
-                <h1 className="text-center text-[25px] font-semibold mb-2 capitalize text-coklat">
+                <h1 className="text-center text-[25px] font-semibold mb-2 capitalize">
                     {title}
                 </h1>
                 <p className="text-center opacity-60 text-[14px] md:text-[16px] ">
